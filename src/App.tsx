3af4c04613125249eacafdbbcb18595e3bf5f864
David <nundavid99@gmail.com>
2022-11-21 16:09:10 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Text } from 'native-base';
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+const App = () => {
+	return(
+		<SafeAreaView>
+			<Text>Hi!</Text>
+		</SafeAreaView>
+	);
+};
+export default App;
