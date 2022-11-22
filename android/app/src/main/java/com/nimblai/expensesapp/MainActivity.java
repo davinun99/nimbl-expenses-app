bd99c0256ab66c4dd9cd5b96d5b2927c21b47edd
David <nundavid99@gmail.com>
2022-11-22 08:20:29 -0300

    new package name on android

--- android/app/src/main/java/com/nimblai/expensesapp/MainActivity.java
+++ android/app/src/main/java/com/nimblai/expensesapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.expensesapp;
+package com.nimblai.expensesapp;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
--- android/app/src/main/java/com/nimblai/expensesapp/MainApplication.java
+++ android/app/src/main/java/com/nimblai/expensesapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.expensesapp;
+package com.nimblai.expensesapp;
 
 import android.app.Application;
 import android.content.Context;
