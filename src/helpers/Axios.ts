336505b56aa29e2b7286a3cde217f01e6474fc6f
David <nundavid99@gmail.com>
2022-11-22 12:34:36 -0300

    Deliting data from storage and Stg backend

--- src/helpers/Axios.ts
+++ src/helpers/Axios.ts
@@ -1,13 +1,15 @@
 import axios, { AxiosError } from 'axios';
 import { Alert } from 'react-native';
+import { AUTH_OBJ_KEY, deleteDataFromStorage } from './AsyncStorage';
 // import * as RootNavigation from '../navigator/RootNavigator';
 
 //PROD
 // const BACKEND_URL = 'https://fkaayuurf1.execute-api.eu-west-2.amazonaws.com/api';
 //DEV
 // const BACKEND_URL = 'https://dev.tarjetaazul.com.py/api';
-// const BACKEND_URL = 'https://ckk9quvsne.execute-api.eu-west-2.amazonaws.com/api'
-const BACKEND_URL = 'http://localhost:3333/api';
+const BACKEND_URL =
+	'https://ckk9quvsne.execute-api.eu-west-2.amazonaws.com/api';
+// const BACKEND_URL = 'http://localhost:3333/api';
 
 const axiosClient = axios.create({
 	baseURL: BACKEND_URL,
@@ -22,6 +24,7 @@ const axiosClient = axios.create({
 const sessionCheckingInterceptor = async (error: AxiosError) => {
 	if (error.response?.status === 401) {
 		Alert.alert('Your session has expired, please re-login');
+		deleteDataFromStorage(AUTH_OBJ_KEY);
 	} else if (error.response?.status === 403) {
 		Alert.alert(
 			`You dont have pemission to invoke this endpoint: '${error.response.config.url}'`,
