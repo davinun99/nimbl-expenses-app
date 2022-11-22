4890815a01dcf504b3e7d0c86e5c1134586ce99f
David <nundavid99@gmail.com>
2022-11-22 10:51:42 -0300

    AuthContext and login initial screen

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,98 @@
+import React, { createContext, useState } from 'react';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import {
+	AUTH_OBJ_KEY,
+	getDataFromStorage,
+	storeData,
+} from '../helpers/AsyncStorage';
+import axiosClient from '../helpers/Axios';
+import { UserInfo } from '../helpers/types';
+import { Alert } from 'react-native';
+
+GoogleSignin.configure({
+	// scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+	webClientId:
+		'973338625449-r85akl9t26re0sj9uu41utarh2pe88cp.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+	// androidClientId: '973338625449-r85akl9t26re0sj9uu41utarh2pe88cp.apps.googleusercontent.com',
+	// offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+	// hostedDomain: '', // specifies a hosted domain restriction
+	// forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+	// accountName: '', // [Android] specifies an account name on the device that should be used
+	iosClientId:
+		'973338625449-vmmin1ou4k1951osd89n1tttma418otl.apps.googleusercontent.com', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+	// googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
+	// openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
+	// profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
+});
+
+interface AuthContextInterface {
+	user: any;
+	isAuthenticated: boolean;
+	errorMessage: string;
+	isAuthLoading: boolean;
+	getExistingUserSession: Function;
+	googleLogin: () => void;
+}
+
+export const AuthContext = createContext({} as AuthContextInterface);
+
+interface AuthProviderProps {
+	children: React.ReactNode;
+}
+
+const AuthProvider = (props: AuthProviderProps) => {
+	const [user, setUser] = useState(null);
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
+	const [isAuthLoading, setIsAuthLoading] = useState(false);
+
+	const afterLoginHandler = async (userInfo: UserInfo) => {
+		storeData(AUTH_OBJ_KEY, userInfo);
+		const token = userInfo.tokens.accessToken;
+		axiosClient.defaults.headers.common.Authorization = `Bearer ${token}`;
+		setUser(user);
+		setIsAuthenticated(true);
+	};
+
+	const getExistingUserSession = async () => {
+		const authData: UserInfo = await getDataFromStorage(AUTH_OBJ_KEY);
+		if (authData) {
+			afterLoginHandler(authData);
+		} else {
+			setErrorMessage('No token found');
+		}
+	};
+	const googleLogin = async () => {
+		setIsAuthLoading(true);
+		try {
+			const hasPlayServices = await GoogleSignin.hasPlayServices();
+			if (hasPlayServices) {
+				const googleUser = await GoogleSignin.signIn();
+				const tokens = await GoogleSignin.getTokens();
+				const userInfo: UserInfo = {
+					...googleUser,
+					tokens,
+				};
+				afterLoginHandler(userInfo);
+			}
+		} catch (error) {
+			Alert.alert('Error logging in, please try again later.');
+		}
+		setIsAuthLoading(false);
+	};
+	return (
+		<AuthContext.Provider
+			value={{
+				user,
+				isAuthenticated,
+				errorMessage,
+				isAuthLoading,
+				getExistingUserSession,
+				googleLogin,
+			}}>
+			{props.children}
+		</AuthContext.Provider>
+	);
+};
+
+export default AuthProvider;
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,45 +1,11 @@
-import React from 'react';
-import {
-	GoogleSignin,
-	GoogleSigninButton,
-	// statusCodes,
-} from '@react-native-google-signin/google-signin';
-import { Center, Box, Heading, VStack } from 'native-base';
-import { Alert, StyleSheet } from 'react-native';
-import { UserInfo } from '../helpers/types';
-GoogleSignin.configure({
-	// scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-	webClientId:
-		'973338625449-r85akl9t26re0sj9uu41utarh2pe88cp.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
-	// androidClientId: '973338625449-r85akl9t26re0sj9uu41utarh2pe88cp.apps.googleusercontent.com',
-	// offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-	// hostedDomain: '', // specifies a hosted domain restriction
-	// forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
-	// accountName: '', // [Android] specifies an account name on the device that should be used
-	iosClientId:
-		'973338625449-vmmin1ou4k1951osd89n1tttma418otl.apps.googleusercontent.com', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
-	// googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
-	// openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
-	// profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
-});
+import React, { useContext } from 'react';
+import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import { Center, Box, Heading, VStack, Spinner } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { AuthContext } from '../context/AuthContext';
 
 const Login = () => {
-	const handleLogIn = async () => {
-		try {
-			const hasPlayServices = await GoogleSignin.hasPlayServices();
-			if (hasPlayServices) {
-				const user = await GoogleSignin.signIn();
-				const tokens = await GoogleSignin.getTokens();
-				const userInfo: UserInfo = {
-					...user,
-					tokens,
-				};
-				console.log(userInfo);
-			}
-		} catch (error) {
-			Alert.alert('Error logging in, please try again later.');
-		}
-	};
+	const { isAuthLoading, googleLogin } = useContext(AuthContext);
 	return (
 		<Center w="100%" h="90%">
 			<Box safeArea p="2" py="8" w="90%" maxW="290" alignItems="center">
@@ -64,13 +30,17 @@ const Login = () => {
 				</Heading>
 
 				<VStack space={3} mt="5" mx="auto">
-					<GoogleSigninButton
-						style={s.googleBtn}
-						size={GoogleSigninButton.Size.Wide}
-						color={GoogleSigninButton.Color.Dark}
-						onPress={handleLogIn}
-						// disabled={this.state.isSigninInProgress}
-					/>
+					{isAuthLoading ? (
+						<Spinner />
+					) : (
+						<GoogleSigninButton
+							style={s.googleBtn}
+							size={GoogleSigninButton.Size.Wide}
+							color={GoogleSigninButton.Color.Dark}
+							onPress={googleLogin}
+							// disabled={this.state.isSigninInProgress}
+						/>
+					)}
 				</VStack>
 			</Box>
 		</Center>
