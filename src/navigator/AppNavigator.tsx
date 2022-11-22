2c1242e30f4536c48f021b1d80358d686e0d9ec7
David <nundavid99@gmail.com>
2022-11-22 08:55:35 -0300

    Login Screen with App Navigator

--- src/navigator/AppNavigator.tsx
+++ src/navigator/AppNavigator.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/Home';
+import LoginScreen from '../screens/Login';
+
 export type AppStackParamList = {
 	HomeScreen: undefined;
 	// Profile: { userId: string };
@@ -11,6 +13,7 @@ const AppStack = createNativeStackNavigator<AppStackParamList>();
 
 const AppNavigator = () => (
 	<AppStack.Navigator>
+		<AppStack.Screen name="LoginScreen" component={LoginScreen} />
 		<AppStack.Screen name="HomeScreen" component={HomeScreen} />
 	</AppStack.Navigator>
 );
