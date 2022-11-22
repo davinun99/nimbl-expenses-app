6de9008988c9ec6e2224f5aca1b0c34ac064d60c
David <nundavid99@gmail.com>
2022-11-22 10:52:36 -0300

    Initial Home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,13 @@
+import { Text } from 'native-base';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+const Home = () => {
+	return (
+		<SafeAreaView>
+			<Text fontSize="lg">Hi! from Home!</Text>
+		</SafeAreaView>
+	);
+};
+
+export default Home;
