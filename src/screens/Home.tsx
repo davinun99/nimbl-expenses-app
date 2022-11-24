180d8e9fda6ad13146cbc022432eb626b7c31404
David <nundavid99@gmail.com>
2022-11-24 18:06:59 -0300

    Home with logic delegated to contexts

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,70 +1,54 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Box, Fab, FlatList, Heading, Icon, Spinner } from 'native-base';
-import React from 'react';
-import { Alert } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
 import { Plus } from 'react-native-feather';
 
 import ExpenseCard from '../components/ExpenseCard';
-import axiosClient from '../helpers/Axios';
+import { ExpenseContext } from '../context/ExpenseContext';
 import { Expense } from '../helpers/types';
 import { AppStackParamList } from '../navigator/AppNavigator';
 
 export type HomeProps = NativeStackScreenProps<AppStackParamList, 'HomeScreen'>;
-
-type HomeState = {
-	expenses: Array<Expense>;
-	isLoading: boolean;
-};
-
-class Home extends React.Component<HomeProps, HomeState> {
-	state = {
-		expenses: [],
-		isLoading: false,
-	};
-	constructor(props: HomeProps) {
-		super(props);
-		this.state = {
-			...this.state,
-			expenses: [],
-			isLoading: true,
+const Home = ({ navigation }: HomeProps) => {
+	const { expenses, expensesAreLoading, getExpenses } =
+		useContext(ExpenseContext);
+	const [isInitialLoad, setIsInitialLoad] = useState(true);
+	useEffect(() => {
+		const initCmp = async () => {
+			await getExpenses();
+			setIsInitialLoad(false);
 		};
-		axiosClient
-			.get('/expenses')
-			.then(resp => {
-				this.setState({
-					...this.state,
-					isLoading: false,
-					expenses: Array.isArray(resp.data) ? resp.data : [],
-				});
-			})
-			.catch(error => {
-				Alert.alert(`Error getting expenses: ${error.message || ''}`);
-			});
-	}
+		initCmp();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-	render() {
-		return (
-			<Box px="5" h="90%">
-				<Heading fontSize="xl" py="4">
-					Expenses
-				</Heading>
-				{this.state.isLoading && <Spinner size="lg" />}
-				<FlatList<Expense>
-					data={this.state.expenses}
-					renderItem={item => <ExpenseCard expense={item.item} />}
-					keyExtractor={item => `expense-${item.expense_id}`}
-				/>
-				<Fab
-					size="sm"
-					icon={<Icon color="white" as={<Plus />} size="sm" />}
-					renderInPortal={false}
-					onPress={() =>
-						this.props.navigation.navigate('CreateExpenseScreen')
-					}
-				/>
-			</Box>
-		);
-	}
-}
-
+	return (
+		<Box px="5" h="100%">
+			<Heading fontSize="xl" py="4">
+				Expenses
+			</Heading>
+			{isInitialLoad && <Spinner color="white" />}
+			<FlatList<Expense>
+				data={expenses}
+				renderItem={item => <ExpenseCard expense={item.item} />}
+				keyExtractor={item => `expense-${item.expense_id}`}
+				onRefresh={getExpenses}
+				refreshing={expensesAreLoading}
+				contentContainerStyle={s.listContentContainer}
+			/>
+			<Fab
+				size="sm"
+				icon={<Icon color="white" as={<Plus />} size="sm" />}
+				renderInPortal={false}
+				onPress={() => navigation.navigate('CreateExpenseScreen')}
+			/>
+		</Box>
+	);
+};
+const s = StyleSheet.create({
+	listContentContainer: {
+		paddingBottom: 45,
+	},
+});
 export default Home;
