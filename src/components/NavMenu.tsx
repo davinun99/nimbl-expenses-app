becc2256c031b1dbc1c18b9ccaa2ed78c9f70f7c
David <nundavid99@gmail.com>
2022-11-26 12:51:35 -0300

    New hamburguer nav menu

---
+++ src/components/NavMenu.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Pressable } from 'react-native';
+import { HamburgerIcon, Menu, Box } from 'native-base';
+import { HeaderButtonProps } from '@react-navigation/native-stack/lib/typescript/src/types';
+// import { AppStackParamList } from '../navigator/AppNavigator';
+
+type Props = HeaderButtonProps & {
+	navigation: any; //Navigation param
+};
+
+const NavMenu = ({ navigation }: Props) => {
+	const navObjs = [
+		{ name: 'Home', onPress: () => navigation?.navigate('HomeScreen') },
+		{
+			name: 'Expenses',
+			onPress: () => navigation?.navigate('ExpensesScreen'),
+		},
+		{
+			name: 'Create expense form',
+			onPress: () => navigation?.navigate('CreateExpenseScreen'),
+		},
+		{
+			name: 'Log out',
+			onPress: () => navigation?.navigate('LogoutScreen'),
+		},
+	];
+	return (
+		<Box>
+			<Menu
+				w="200"
+				trigger={triggerProps => (
+					<Pressable
+						accessibilityLabel="More options menu"
+						{...triggerProps}>
+						<Box
+							w={10}
+							h={10}
+							display="flex"
+							justifyContent="center"
+							alignItems="center">
+							<HamburgerIcon />
+						</Box>
+					</Pressable>
+				)}>
+				{navObjs.map((route, i) => (
+					<Menu.Item key={`menuItem-${i}`} onPress={route.onPress}>
+						{route.name}
+					</Menu.Item>
+				))}
+			</Menu>
+		</Box>
+	);
+};
+
+export default NavMenu;
