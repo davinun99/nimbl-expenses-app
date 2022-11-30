053d22df1165086b4a285bd5b835900f11d0223d
David <nundavid99@gmail.com>
2022-11-30 08:22:09 -0300

    new date utils and month names array

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,19 +1,45 @@
 import RNFetchBlob from 'rn-fetch-blob';
 import { BackendFile } from './types';
 
+export const monthNamesArray = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
+
 /**
- * It takes a string in the format of YYYY-MM-DD and returns a string in the format of MM/DD/YYYY
- * @param {string} strDate - The string date to be converted to a formatted date.
- * @returns A string that is the date in the format of "MM/DD/YYYY"
+ * It takes a string in the format of YYYY-MM-DD and returns a Date object
+ * @param {string} strDate - The string date to convert to a Date object.
+ * @returns The date object obtained or null
  */
-export const strToFormattedDate = (strDate: string) => {
+export const convertStrToDate = (strDate: string) => {
 	if (typeof strDate !== 'string' || strDate.length < 10) {
-		return '';
+		return null;
 	}
+	//YYYY-MM-DD date
 	const year = Number(strDate.substring(0, 4));
 	const month = Number(strDate.substring(5, 7));
 	const day = Number(strDate.substring(8, 10));
-	return new Date(year, month - 1, day).toLocaleDateString();
+	return new Date(year, month - 1, day);
+};
+
+/**
+ * It takes a string in the format of YYYY-MM-DD and returns a string in the format of MM/DD/YYYY
+ * @param {string} strDate - The string date to be converted to a formatted date.
+ * @returns A string that is the date in the format of "MM/DD/YYYY" or ""
+ */
+export const strToFormattedDate = (strDate: string) => {
+	const date = convertStrToDate(strDate);
+	return date?.toLocaleDateString() || '';
 };
 
 /**
