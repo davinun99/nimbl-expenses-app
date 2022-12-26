30e84440aad30b3399468743eb9e8352227dcd2f
David <nundavid99@gmail.com>
2022-12-26 19:13:18 -0300

    Similar expenses ux/ui

--- src/screens/CreateExpenseSteps/CreateExpenseWithCamera.tsx
+++ src/screens/CreateExpenseSteps/CreateExpenseWithCamera.tsx
@@ -7,6 +7,7 @@ import {
 	Select,
 	Button,
 	HStack,
+	useDisclose,
 } from 'native-base';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import ExpensePhotoPreview from '../../components/ExpensePhotoFilePreview';
@@ -19,6 +20,8 @@ import { CreateExpenseStackParamList } from '../../navigator/CreateExpenseNaviga
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { convertStrToDate } from '../../helpers';
 import { PayMethodContext } from '../../context/PaymentMethodContext';
+import ModalSimilarExpenses from '../../components/ModalSimilarExpenses';
+import { Expense } from '../../helpers/types';
 
 type expensePropertyName =
 	| 'expense_description'
@@ -36,6 +39,7 @@ type Props = NativeStackScreenProps<
 
 const CreateExpenseWithCamera = ({ navigation }: Props) => {
 	const [showError, setShowError] = useState(false);
+	const [similarExpenses, setSimilarExpenses] = useState<Expense[]>([]);
 	const { categories } = useContext(ExpendCategoryContext);
 	const { paymentMethods } = useContext(PayMethodContext);
 	const {
@@ -45,10 +49,13 @@ const CreateExpenseWithCamera = ({ navigation }: Props) => {
 		getExpenses,
 		setNewExpense,
 		createExpense,
+		updateExpense,
 		setExpenseSnapshot,
 		validateExpense,
 		cleanExpense,
+		getSimilarExpenses,
 	} = useContext(ExpenseContext);
+	const similarExpensesModal = useDisclose();
 	const handleChange = (name: expensePropertyName, value: string) => {
 		setNewExpense({ ...newExpense, [name]: value });
 	};
@@ -65,6 +72,12 @@ const CreateExpenseWithCamera = ({ navigation }: Props) => {
 		if (!newExpense.amount) {
 			newExpense.amount = null;
 		}
+		const _similarExpenses = getSimilarExpenses(newExpense);
+		if (_similarExpenses.length) {
+			setSimilarExpenses(_similarExpenses);
+			similarExpensesModal.onOpen();
+			return;
+		}
 		const isCompleted = await createExpense(newExpense);
 		if (isCompleted) {
 			getExpenses();
@@ -76,157 +89,191 @@ const CreateExpenseWithCamera = ({ navigation }: Props) => {
 			cleanExpense();
 		}
 	};
+	const handleMatchExpense = async (matchExpenseId: number) => {
+		let isCompleted = false;
+		if (!matchExpenseId) {
+			//No match, just create an expense
+			isCompleted = await createExpense(newExpense);
+		} else {
+			//Match, update existing
+			isCompleted = await updateExpense(newExpense, matchExpenseId);
+		}
+		if (isCompleted) {
+			getExpenses();
+			Alert.alert('Succesfully created expense!');
+			navigation.reset({
+				index: 0,
+				routes: [{ name: 'ExpensesScreen' }],
+			});
+			cleanExpense();
+		}
+		return isCompleted;
+	};
 	const handleClosePhoto = () => {
 		navigation.navigate('FullScreenCamera');
 		setExpenseSnapshot(null);
 	};
 	const isValid = validateExpense();
 	return (
-		<KeyboardAwareScrollView
-			enableOnAndroid
-			enableAutomaticScroll
-			keyboardOpeningTime={0}
-			extraHeight={Platform.select({ android: 200 })}>
-			<Box px="5" w="100%" h="100%" bgColor="white" mt={5}>
-				<AlertComponent
-					show={showError}
-					setShow={setShowError}
-					title={`Error creating expense: ${expenseErrorMessage}`}
-					status="error"
-				/>
-				<ExpensePhotoPreview handleClosePhoto={handleClosePhoto} />
-				<VStack mb={10}>
-					{/* Description */}
-					<FormControl>
-						<FormControl.Label
-							_text={{
-								fontSize: '16',
-							}}>
-							Expense description
-						</FormControl.Label>
-						<Input
-							placeholder="Travel to Malaga"
-							onChangeText={value =>
-								handleChange('expense_description', value)
-							}
-							bgColor="white"
-							size="md"
-						/>
-					</FormControl>
-					{/* Category */}
-					<FormControl mt={3} isRequired>
-						<FormControl.Label
-							_text={{
-								fontSize: '16',
-							}}>
-							Category
-						</FormControl.Label>
-						<Select
-							selectedValue={`${newExpense.expense_category_id}`}
-							size="md"
-							bgColor="white"
-							placeholder="Select a category"
-							onValueChange={itemValue =>
-								handleChange('expense_category_id', itemValue)
-							}>
-							{categories.map(cat => (
-								<Select.Item
-									key={`expense-category-${cat.expense_category_id}`}
-									label={cat.expense_category_description}
-									value={`${cat.expense_category_id}`}
-								/>
-							))}
-						</Select>
-					</FormControl>
-					{/* Date picker */}
-					<FormControl mt={3}>
-						<FormControl.Label
-							_text={{
-								fontSize: '16',
-							}}>
-							Date
-						</FormControl.Label>
-						<InlineDatePicker
-							value={
-								convertStrToDate(newExpense.expense_date) ||
-								new Date()
-							}
-							setValue={setExpDate}
-							placeholder={newExpense.expense_date}
-						/>
-					</FormControl>
-					{/* Amount and currency */}
-					<HStack mt={3}>
-						<FormControl w="60%" mr="5%">
+		<>
+			<ModalSimilarExpenses
+				showModal={similarExpensesModal.isOpen}
+				toggleModal={similarExpensesModal.onClose}
+				similarExpenses={similarExpenses}
+				handleMatchExpense={handleMatchExpense}
+			/>
+			<KeyboardAwareScrollView
+				enableOnAndroid
+				enableAutomaticScroll
+				keyboardOpeningTime={0}
+				extraHeight={Platform.select({ android: 200 })}>
+				<Box px="5" w="100%" h="100%" bgColor="white" mt={5}>
+					<AlertComponent
+						show={showError}
+						setShow={setShowError}
+						title={`Error creating expense: ${expenseErrorMessage}`}
+						status="error"
+					/>
+					<ExpensePhotoPreview handleClosePhoto={handleClosePhoto} />
+					<VStack mb={10}>
+						{/* Description */}
+						<FormControl>
 							<FormControl.Label
 								_text={{
 									fontSize: '16',
 								}}>
-								Expense amount
+								Expense description
 							</FormControl.Label>
 							<Input
-								placeholder="100"
+								placeholder="Travel to Malaga"
 								onChangeText={value =>
-									handleChange('amount', value)
+									handleChange('expense_description', value)
 								}
 								bgColor="white"
-								keyboardType="numeric"
-								size="lg"
+								size="md"
 							/>
 						</FormControl>
-						<FormControl w="35%" isRequired>
+						{/* Category */}
+						<FormControl mt={3} isRequired>
 							<FormControl.Label
 								_text={{
 									fontSize: '16',
 								}}>
-								Currency
+								Category
 							</FormControl.Label>
 							<Select
-								onValueChange={value =>
-									handleChange('expense_currency', value)
-								}
-								selectedValue={newExpense.expense_currency}
-								size="lg"
-								bgColor="white">
-								<Select.Item label="EUR" value="EUR" />
-								<Select.Item label="USD" value="USD" />
+								selectedValue={`${newExpense.expense_category_id}`}
+								size="md"
+								bgColor="white"
+								placeholder="Select a category"
+								onValueChange={itemValue =>
+									handleChange(
+										'expense_category_id',
+										itemValue,
+									)
+								}>
+								{categories.map(cat => (
+									<Select.Item
+										key={`expense-category-${cat.expense_category_id}`}
+										label={cat.expense_category_description}
+										value={`${cat.expense_category_id}`}
+									/>
+								))}
 							</Select>
 						</FormControl>
-					</HStack>
-					{/* Payment Method select */}
-					<FormControl my={4} isRequired>
-						<FormControl.Label
-							_text={{
-								fontSize: '16',
-							}}>
-							Payment Method
-						</FormControl.Label>
-						<Select
-							selectedValue={`${newExpense.expense_pay_method_id}`}
-							size="lg"
-							bgColor="white"
-							placeholder="Select a payment method"
-							onValueChange={itemValue =>
-								handleChange('expense_pay_method_id', itemValue)
-							}>
-							{paymentMethods.map(p => (
-								<Select.Item
-									key={`expense-category-${p.payment_method_id}`}
-									label={p.card_alias}
-									value={`${p.payment_method_id}`}
+						{/* Date picker */}
+						<FormControl mt={3}>
+							<FormControl.Label
+								_text={{
+									fontSize: '16',
+								}}>
+								Date
+							</FormControl.Label>
+							<InlineDatePicker
+								value={
+									convertStrToDate(newExpense.expense_date) ||
+									new Date()
+								}
+								setValue={setExpDate}
+								placeholder={newExpense.expense_date}
+							/>
+						</FormControl>
+						{/* Amount and currency */}
+						<HStack mt={3}>
+							<FormControl w="60%" mr="5%">
+								<FormControl.Label
+									_text={{
+										fontSize: '16',
+									}}>
+									Expense amount
+								</FormControl.Label>
+								<Input
+									placeholder="100"
+									onChangeText={value =>
+										handleChange('amount', value)
+									}
+									bgColor="white"
+									keyboardType="numeric"
+									size="lg"
 								/>
-							))}
-						</Select>
-					</FormControl>
-					<Button
-						onPress={handleSave}
-						isLoading={expensesAreLoading}
-						isDisabled={!isValid}>
-						Save expense
-					</Button>
-				</VStack>
-			</Box>
-		</KeyboardAwareScrollView>
+							</FormControl>
+							<FormControl w="35%" isRequired>
+								<FormControl.Label
+									_text={{
+										fontSize: '16',
+									}}>
+									Currency
+								</FormControl.Label>
+								<Select
+									onValueChange={value =>
+										handleChange('expense_currency', value)
+									}
+									selectedValue={newExpense.expense_currency}
+									size="lg"
+									bgColor="white">
+									<Select.Item label="EUR" value="EUR" />
+									<Select.Item label="USD" value="USD" />
+								</Select>
+							</FormControl>
+						</HStack>
+						{/* Payment Method select */}
+						<FormControl my={4} isRequired>
+							<FormControl.Label
+								_text={{
+									fontSize: '16',
+								}}>
+								Payment Method
+							</FormControl.Label>
+							<Select
+								selectedValue={`${newExpense.expense_pay_method_id}`}
+								size="lg"
+								bgColor="white"
+								placeholder="Select a payment method"
+								onValueChange={itemValue =>
+									handleChange(
+										'expense_pay_method_id',
+										itemValue,
+									)
+								}>
+								{paymentMethods.map(p => (
+									<Select.Item
+										key={`expense-category-${p.payment_method_id}`}
+										label={p.card_alias}
+										value={`${p.payment_method_id}`}
+									/>
+								))}
+							</Select>
+						</FormControl>
+						<Button
+							onPress={handleSave}
+							isLoading={expensesAreLoading}
+							isDisabled={!isValid}>
+							Save expense
+						</Button>
+					</VStack>
+				</Box>
+			</KeyboardAwareScrollView>
+		</>
 	);
 };
 
